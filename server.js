821e354094fcf29a033c55390c024457346e513c
Meet Soni <meetsoni3017@gmail.com>
2023-04-22 17:12:44 +0530

    migration of database

--- server.js
+++ server.js
@@ -1,40 +1,64 @@
-require('dotenv').config()
-const express = require('express');
-const mysql = require('mysql');
-const app = express();
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+import dotenv from 'dotenv';
+dotenv.config();
+import express from "express";
+import { connect, Schema, model } from "mongoose";
+import { hash as _hash, compare } from "bcrypt";
 const saltRounds = 10;
+import { createTransport } from 'nodemailer';
+import { generate } from 'otp-generator';
+import ejs from 'ejs';
 
-const nodemailer = require('nodemailer');
-const otpGenerator = require('otp-generator');
+const app = express();
 
+app.engine('html', ejs.renderFile);
+app.use(express.urlencoded({ extended: true }));
 
-app.engine('html', require('ejs').renderFile);
-app.use(bodyParser.urlencoded({ extended: true }));
+connect(
+  "mongodb://127.0.0.1:27017/taskDB",
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Successsfully connected to the database"))
+  .catch((err) => { console.log(err); });
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: process.env.DBPASSWORD,
-  database: 'taskdb'
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  userName: String,
+  email: String,
+  password: String,
+  totalTasks: Number,
+  completeTasks: Number,
+  otp: String
 });
 
-
-db.connect((err) => {
-  if (err) {
-    throw err;
-    console.log(err);
-  }
-  console.log('Connected to MySQL database');
+const taskSchema = new Schema({
+  _id: String,
+  email: String,
+  description: String,
+  start: String,
+  end: String,
+  subTasks: String,
+  status: String,
+  isCompleted: Boolean,
+  priority: Boolean,
 });
 
-app.get('/register', function(req, res){
-  res.render('index.html');
+const subTaskSchema = new Schema({
+  _id: String,
+  task_id: String,
+  subDescription: String,
+  isComplete: Boolean, 
 });
 
-app.get('/login', function(req, res){
-  res.render('login.html')
+const User = new model("User", userSchema);
+const Task = new model("Task", taskSchema);
+
+
+app.get('/register', function (req, res) {
+  res.render('register.html');
+})
+
+app.get('/login', function (req, res) {
+  res.render('login.html');
 })
 
 app.get('/forgot-password', function(req, res){
@@ -45,170 +69,158 @@ app.get('/reset-password', function(req, res){
   res.render('reset-password.html')
 })
 
-app.post('/register', (req, res) => {
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const email = req.body.email;
-    const password = hash;
-
-    const query = `INSERT INTO user (email, password) VALUES ('${email}', '${password}')`;
-
-    db.query(query, (err, result) => {
-      if (err) {
-        console.error('Error inserting user into MySQL database:', err);
-        res.status(500).send('Error inserting user into MySQL database');
-      } else {
-        console.log('User inserted into MySQL database:', result);
-        res.send('User registered successfully');
-      }
-    });
+app.post('/register', function (req, res) {
+  _hash(req.body.password, saltRounds, async (err, hash) => {
+    const found = await User.find({ email: req.body.email });
+    if (found.length > 0) {
+      res.send({
+        "status_code": 206,
+        "failure": "Email exits"
+      });
+    }
+    else {
+      const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userName: req.body.userName,
+        email: req.body.email,
+        password: hash,
+        totalTasks: 0,
+        completeTasks: 0,
+        otp: ''
+      });
+      
+      newUser.save()
+      .then(() => 
+        res.send("Register")
+      )
+      .catch((err) => { 
+        console.log(err); 
+      });0
+    }
   });
 });
+app.post('/login', async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
 
-app.post('/login', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+  try {
+    const foundUser = await User.findOne({ email: email });
+    if (foundUser) {
+      const comparision = await compare(password, foundUser.password);
+      console.log("Compa: ", comparision)
+      console.log("Pass", password)
 
-    const query = `SELECT * from taskdb.user WHERE email = '${email}'`;
-
-    db.query(query, async (err, result, field) => {
-      if (err) {
+      if (comparision) {
+        console.log("Matches");
         res.send({
           "code": 200,
-          "failed": "Error occured"
-        })
-      } else {
-        // 2 cases here : 1) email not found, 2) password doesn't match
-        // 1) redirect user to Register
-        // 2) Password incorrect
-        if(result.length>0){
-
-        const comparision = await bcrypt.compare(password, result[0].password);
-          if(comparision){
-            console.log(result)
-            res.send({
-              "code": 200,
-              "success": "Login Successful"
-            })
-          }
-          else{
-            res.send({
-                       "code":204,
-                       "success":"Email and password does not match"
-                  })
-          }
-        }
-        else{
-          res.send({
-            "code":206,
-            "success":"Email does not exits"
-              });
-        }
+          "message": "Success"
+        });
       }
-    });
-  })
-
-app.get('/users', (req, res) => {
-  const sql = 'SELECT * FROM taskdb.user';
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
+      else {
+        res.send({"code": 401,
+         "meassage": "Password does not match"
+        });
+      }
     }
-    res.send(result);
-  });
+    else {
+      res.send({"code": 206,
+       "meassge": 'User not found'
+      });
+    }
+  }
+  catch {
+    res.send({"code": 206,
+     "message": "user not found"
+    });
+  }
 });
 
 // Endpoint concerned with handling forgot password requests
-app.post("/forgot-password", async (req,res) => {
+app.post("/forgot-password", async (req, res) => {
   const email = req.body.email;
-
-  const query = `SELECT * from taskdb.user WHERE email = '${email}'`;
-
-  db.query(query, (err,result) => {
-    if(err) {
-      console.log("No registered User was found with given Email");
-      return res.status(400).send("not found");
-    }
-    if(result.length === 0) 
-      return res.status(400).send("user not found");
-  })
-
-  const otp = otpGenerator.generate(6, { upperCase: false, specialChars: false });
-
-  sqlQuery = `UPDATE taskdb.user SET otp = '${otp}' WHERE email='${email}'`;
-  db.query(sqlQuery, (err, result)=>{
-    if(err){
-      console.log(err)
-    }
-  })
-  
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      // this username and password is the one from which email will be sent
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    }
-  });
-
-  const mailOptions = {
-    from: process.env.EMAIL_USERNAME,
-    to: email,
-    subject: 'Reset your password',
-    text: `Your password reset OTP is ${otp}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      return res.status(500).send('Failed to send email');
-    }
-
-    console.log('Email sent: ' + info.response);
-    res.send('Password reset OTP sent');
-  });
-});
-
-// Endpoint concerned with handling password reset requests via Otp verification
-app.post("/reset-password", async (req,res) => {
-  const { email, otp, password } = req.body;
-  bcrypt.hash(password, saltRounds, function(err, hash){
-    if(err) throw err;
-    const newPassword = hash;
+  const found = await User.findOne({email: email});
+  if(!found) {
+    return res.status(500).send('User not found');
+  }
   try {
-    // verify otp
-    const qry = `SELECT otp from taskdb.user where email = '${email}'`;
-    db.query(qry, async (err,ress) => {
-      console.log(ress)
-      if(err) throw err;
-      else {
-        if(ress[0].otp === otp) {
+    const otp = generate(6, { upperCase: false, specialChars: false });
+    const addOtp = await User.findOneAndUpdate({ email: email }, { otp: otp })
 
-          db.query(`UPDATE user SET password = '${newPassword}' WHERE email='${email}'`, (req,result) => {
-            if (err) throw err;
-            else {
-              console.log("Password updated successfully",result);
-            }
-          });
-
-          //deleting Otp after updating password
-          db.query(`UPDATE user SET otp = '' WHERE email='${email}'`);
-          res.send("Password updated successfully");
-        }
-        else {
-          res.send("wrong otp");
-        }
+    const transporter = createTransport({
+      service: 'gmail',
+      secure: false,
+      auth: {
+        // this username and password is the one from which email will be sent
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
       }
     });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: email,
+      subject: 'Reset your password',
+      text: `Your password reset OTP is ${otp}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send('Failed to send email');
+      }
+      console.log('Email sent: ' + info.response);
+      res.send('Password reset OTP sent');
+    });
   }
-  catch(error) {
-    console.log(error);
-    res.status(401).send('OTP is invalid or has expired');
+  catch {
+    res.send("User not found");
   }
-})
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(process.env.PORT, () => {
-  console.log('Server started on port 3000');
+app.post("/reset-password", async (req,res) => {
+  const { email, otp, password } = req.body;
+  _hash(password, saltRounds, async function(err, hash){
+    if(err) throw err;
+    const newPassword = hash;
+    try {
+      // verify otp
+      const found = await User.findOne({email: email});
+      if(found.otp === otp) {
+        
+        await User.findOneAndUpdate({email: email}, {password: newPassword})
+          .then(async () => {
+            await User.findOneAndUpdate({email: email}, {otp: ''})
+            console.log("Password updated successfully");
+          });
+          res.send("Password updated successfully");
+      }
+      else {
+        res.send("wrong otp");
+      }
+    }
+    catch(error) {
+      console.log(error);
+      res.status(401).send('OTP is invalid or has expired');
+    }
+  });
+});
+
+app.get("/get-task", async (req, res) => {
+  const email = req.body.email;
+  const found = await Task.findOne({email: email});
+  console.log(found)
+
+  if(found) {
+    console.log(123);
+    res.send(found);
+  }
+  else {
+    res.send("User does not exists!");
+  }
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
