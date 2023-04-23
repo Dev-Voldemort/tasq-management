63bff6b391e8c0d58533ea65b9fa6f2d451618b3
Meet Soni <meetsoni3017@gmail.com>
2023-04-23 14:10:23 +0530

    Revert "Revert "optimize register logic""
    This reverts commit 3a95f96766a135f1d5896005ac42eda1c1aa9041.

---
+++ mail/otpValidation.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { createTransport } from "nodemailer";
+import { User } from "../database/database.js";
+
+async function sendOtp(req, res, message, otp) {
+  const email = req.body.email;
+  try {
+    await User.findOneAndUpdate({ email: email }, { otp: otp });
+
+    const transporter = createTransport({
+      service: "gmail",
+      secure: false,
+      auth: {
+        // this username and password is the one from which email will be sent
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: email,
+      subject: message,
+      text: `Your OTP is ${otp}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send("Failed to send email");
+      }
+      console.log("Email sent: " + info.response);
+      res.send("OTP sent successfully!");
+    });
+  } catch {
+    res.send("User not found");
+  }
+}
+
+export { sendOtp };
--- server.js
+++ server.js
@@ -1,84 +1,49 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import { connect, Schema, model } from "mongoose";
+import { generate } from "otp-generator";
+import ejs from "ejs";
 import { hash as _hash, compare } from "bcrypt";
 const saltRounds = 10;
-import { createTransport } from 'nodemailer';
-import { generate } from 'otp-generator';
-import ejs from 'ejs';
+
+import { User } from "./database/database.js";
+import { sendOtp } from "./mail/otpValidation.js";
 
 const app = express();
 
-app.engine('html', ejs.renderFile);
+app.engine("html", ejs.renderFile);
 app.use(express.urlencoded({ extended: true }));
 
-connect(
-  "mongodb://127.0.0.1:27017/taskDB",
-  { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Successsfully connected to the database"))
-  .catch((err) => { console.log(err); });
-
-const userSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  userName: String,
-  email: String,
-  password: String,
-  totalTasks: Number,
-  completeTasks: Number,
-  otp: String
+app.get("/register", function (req, res) {
+  res.render("register.html");
 });
 
-const taskSchema = new Schema({
-  _id: String,
-  email: String,
-  description: String,
-  start: String,
-  end: String,
-  subTasks: String,
-  status: String,
-  isCompleted: Boolean,
-  priority: Boolean,
+app.get("/login", function (req, res) {
+  res.render("login.html");
 });
 
-const subTaskSchema = new Schema({
-  _id: String,
-  task_id: String,
-  subDescription: String,
-  isComplete: Boolean, 
+app.get("/forgot-password", function (req, res) {
+  res.render("forgot-password.html");
 });
 
-const User = new model("User", userSchema);
-const Task = new model("Task", taskSchema);
+app.get("/reset-password", function (req, res) {
+  res.render("reset-password.html");
+});
 
-
-app.get('/register', function (req, res) {
-  res.render('register.html');
-})
-
-app.get('/login', function (req, res) {
-  res.render('login.html');
-})
-
-app.get('/forgot-password', function(req, res){
-  res.render('forgot-password.html')
-})
-
-app.get('/reset-password', function(req, res){
-  res.render('reset-password.html')
-})
-
-app.post('/register', function (req, res) {
+app.post("/register", function (req, res) {
   _hash(req.body.password, saltRounds, async (err, hash) => {
     const found = await User.find({ email: req.body.email });
-    if (found.length > 0) {
+
+    if (found.length > 0 && req.body.isVerified === false) {
       res.send({
-        "status_code": 206,
-        "failure": "Email exits"
+        status_code: 206,
+        message: "Email exists",
       });
-    }
-    else {
+    } else {
+      // storing the OTP in the database for Email validation
+      const otp = generate(6, { upperCase: false, specialChars: false });
+      await sendOtp(req, res, "Verification OTP", otp);
+
       const newUser = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -87,52 +52,95 @@ app.post('/register', function (req, res) {
         password: hash,
         totalTasks: 0,
         completeTasks: 0,
-        otp: ''
+        otp: otp,
+        isVerified: false,
       });
-      
-      newUser.save()
-      .then(() => 
-        res.send("Register")
-      )
-      .catch((err) => { 
-        console.log(err); 
-      });0
+
+      // saving the registered user in database as non verified user
+      newUser
+        .save()
+        .then(() =>
+          res.send({
+            status_code: 200,
+            message: "User registered",
+            body: {
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              userName: req.body.userName,
+              email: req.body.email,
+              totalTasks: newUser.totalTasks,
+              completeTasks: newUser.completeTasks,
+              isVerified: newUser.isVerified,
+            },
+          })
+        )
+        .catch((err) => {
+          console.log(err);
+        });
     }
   });
 });
-app.post('/login', async function (req, res) {
+
+// email validation and otp verification
+app.post("/validate-email", async (req, res) => {
+  const { email, otp } = req.body;
+  const foundUser = await User.findOne({ email: email });
+
+  if (!foundUser) {
+    res.send("User not found");
+  } else {
+    if (foundUser.otp === otp) {
+      // deleting the otp and marking user as verified
+      await User.updateOne(
+        { email: email },
+        { $set: { otp: "", isVerified: true } }
+      );
+      res.send({
+        status_code: 200,
+        message: "Email has been verified successfully",
+      });
+    } else {
+      res.send("wrong otp");
+    }
+  }
+});
+
+app.post("/login", async function (req, res) {
   const email = req.body.email;
   const password = req.body.password;
-
   try {
     const foundUser = await User.findOne({ email: email });
     if (foundUser) {
-      const comparision = await compare(password, foundUser.password);
-      console.log("Compa: ", comparision)
-      console.log("Pass", password)
+      // checking whether the user is verified
+      if(!foundUser.isVerified) {
+        return res.send({
+          message: "User must verify the email",
+        });
+      }
 
-      if (comparision) {
-        console.log("Matches");
-        res.send({
-          "code": 200,
-          "message": "Success"
+      const comparison = await compare(password, foundUser.password);
+
+      if (comparison) {
+        return res.send({
+          status_code: 200,
+          user: foundUser,
+        });
+      } else {
+        return res.send({
+          status_code: 401,
+          message: "Password does not match",
         });
       }
-      else {
-        res.send({"code": 401,
-         "meassage": "Password does not match"
-        });
-      }
-    }
-    else {
-      res.send({"code": 206,
-       "meassge": 'User not found'
+    } else {
+      return res.send({
+        status_code: 206,
+        message: "User not found",
       });
     }
-  }
-  catch {
-    res.send({"code": 206,
-     "message": "user not found"
+  } catch (err) {
+    return res.send({
+      status_code: 206,
+      message: err,
     });
   }
 });
@@ -140,85 +148,40 @@ app.post('/login', async function (req, res) {
 // Endpoint concerned with handling forgot password requests
 app.post("/forgot-password", async (req, res) => {
   const email = req.body.email;
-  const found = await User.findOne({email: email});
-  if(!found) {
-    return res.status(500).send('User not found');
+  const found = await User.findOne({ email: email });
+  if (!found) {
+    return res.status(500).send("User not found");
   }
-  try {
-    const otp = generate(6, { upperCase: false, specialChars: false });
-    const addOtp = await User.findOneAndUpdate({ email: email }, { otp: otp })
-
-    const transporter = createTransport({
-      service: 'gmail',
-      secure: false,
-      auth: {
-        // this username and password is the one from which email will be sent
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      }
-    });
-
-    const mailOptions = {
-      from: process.env.EMAIL_USERNAME,
-      to: email,
-      subject: 'Reset your password',
-      text: `Your password reset OTP is ${otp}`,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).send('Failed to send email');
-      }
-      console.log('Email sent: ' + info.response);
-      res.send('Password reset OTP sent');
-    });
-  }
-  catch {
-    res.send("User not found");
-  }
+  const otp = generate(6, { upperCase: false, specialChars: false });
+  sendOtp(req, res, "Reset password OTP", otp);
 });
 
-app.post("/reset-password", async (req,res) => {
+app.post("/reset-password", async (req, res) => {
+  // email, otp and password that came from front end
   const { email, otp, password } = req.body;
-  _hash(password, saltRounds, async function(err, hash){
-    if(err) throw err;
+
+  _hash(password, saltRounds, async function (err, hash) {
+    if (err) console.log("Reset Password: ", err);
     const newPassword = hash;
     try {
       // verify otp
-      const found = await User.findOne({email: email});
-      if(found.otp === otp) {
-        
-        await User.findOneAndUpdate({email: email}, {password: newPassword})
-          .then(async () => {
-            await User.findOneAndUpdate({email: email}, {otp: ''})
-            console.log("Password updated successfully");
-          });
-          res.send("Password updated successfully");
-      }
-      else {
+      const found = await User.findOne({ email: email });
+      if (found.otp === otp) {
+        await User.findOneAndUpdate(
+          { email: email },
+          { password: newPassword }
+        ).then(async () => {
+          await User.findOneAndUpdate({ email: email }, { otp: "" });
+        });
+        res.send("Password updated successfully");
+      } else {
         res.send("wrong otp");
       }
-    }
-    catch(error) {
+    } catch (error) {
       console.log(error);
-      res.status(401).send('OTP is invalid or has expired');
+      res.status(401).send("OTP is invalid or has expired");
     }
   });
-});
-
-app.get("/get-task", async (req, res) => {
-  const email = req.body.email;
-  const found = await Task.findOne({email: email});
-  console.log(found)
-
-  if(found) {
-    console.log(123);
-    res.send(found);
-  }
-  else {
-    res.send("User does not exists!");
-  }
 });
 
 app.listen(3000, function () {
